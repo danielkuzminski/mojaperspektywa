17685da30c5fcb1d47681204ae257fba5d7bb0cc
danielkuzminski <dankuzmina@gmail.com>
2021-02-20 03:50:35 +0100

    setup project, add firestore and firebase

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Moja Perspektywa</h1>
+      <h2>Galeria</h2>
+      <p>Serdecznie zapraszam</p>
+    </div>
+  )
+}
+
+export default Title;
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyBkDHu0PhhGtvYlqFkc5tETjqA1Bb6WQ0w',
+  authDomain: 'moja-perspektywa.firebaseapp.com',
+  projectId: 'moja-perspektywa',
+  storageBucket: 'moja-perspektywa.appspot.com',
+  messagingSenderId: '891512748652',
+  appId: '1:891512748652:web:d0349632f2b52d51ee3649',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
